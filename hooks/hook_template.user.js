0dd70345837de51a05aff17c4d378ed6d1010b12
Pascal Heitz <heitz.pascal@gmail.com>
2019-01-20 14:22:33 +0100

    updated template

--- hooks/hook_template.user.js
+++ hooks/hook_template.user.js
@@ -1,17 +1,12 @@
 // ==UserScript==
-// @name         WordReference AnkiQuickAdder Hook
-// @namespace    https://github.com/OoDeLally
+// @name         FIXME AnkiQuickAdder Hook
+// @namespace    FIXME
 // @version      0.1
-// @description  Generate a hook for AnkiQuickAdder on WordReference
-// @author       Pascal Heitz
-// @include      /http://www\.wordreference\.com\/[a-z]{4}\/.+/
+// @description  Generate a hook for AnkiQuickAdder on FIXME
+// @author       FIXME
+// @include      FIXME
 // @grant        none
 // ==/UserScript==
-
-function getLanguageCodes() {
-  const match = window.location.href.match(/wordreference\.com\/([a-z]{2})([a-z]{2})\//);
-  return [match[1], match[2]];
-}
 
 
 function appendStyleSheep() {
@@ -63,9 +58,7 @@ function appendStyleSheep() {
 }
 
 
-function createHook(frontText, backText, trGroup) {
-  console.log('frontText:', frontText)
-  console.log('backText:', backText)
+function createHook(frontText, backText) {
   const starNodeBig = document.createElement('div');
   starNodeBig.innerText = '★';
   starNodeBig.className = '--anki-quick-adder-hook--star --anki-quick-adder-hook--big';
@@ -78,12 +71,6 @@ function createHook(frontText, backText, trGroup) {
   hookNode.setAttribute('back', backText);
   hookNode.innerText = 'Add';
   hookNode.title = 'Create an Anki card from this translation';
-  hookNode.onmouseover = () => {
-    trGroup.forEach(tr => { tr.style.backgroundColor = 'gold' });
-  };
-  hookNode.onmouseout = () => {
-    trGroup.forEach(tr => { tr.style.backgroundColor = null });
-  };
   hookNode.onclick = (event) => {
     const hookNode = event.target;
     if (hookNode.dataset.clickHandler) {
@@ -105,104 +92,14 @@ function createHook(frontText, backText, trGroup) {
 }
 
 
-function getTrGroups(tableNode) {
-  const trGroups = [];
-  let currentTrGroup = [];
-  let currentTrClass = 'even';
-  // console.log('tableNode.querySelectorAll():', tableNode.querySelectorAll('.even, .odd'))
-  Array.from(tableNode.querySelectorAll('.even, .odd'))
-  // .sort((a, b) => a.rowIndex - b.rowIndex)
-  .forEach(trNode => {
-    if (trNode.className == currentTrClass) {
-      currentTrGroup.push(trNode);
-    } else {
-      trGroups.push(currentTrGroup);
-      currentTrGroup = [trNode];
-      currentTrClass = currentTrClass == 'even' ? 'odd' : 'even';
-    }
-  });
-  trGroups.push(currentTrGroup);
-  return trGroups;
-}
-
-
-function extractFrontText(trGroup) {
-  const firstRowTds = trGroup[0].querySelectorAll('td');
-  const firstCell = firstRowTds[0];
-  const firstCellStrong = firstCell.querySelector('strong');
-  if (!firstCellStrong) {
-    return // Not a real definition row
-  }
-  const firstCellStrongChildren = Array.from(firstCellStrong.childNodes).filter(node => node.nodeName != 'A');
-  const firstChildText = firstCellStrongChildren.map(node => node.textContent).join('');
-  const remainingChildrenTexts = Array.from(firstCell.childNodes)
-  .slice(1)
-  .map(node => node.innerText)
-  .filter(text => text)
-  .map(text => text.trim())
-  .filter(text => text);
-  let frontText = `${getLanguageCodes()[0].toUpperCase()}\n${firstChildText}`;
-  if (remainingChildrenTexts.length > 0) {
-    frontText += ` [${remainingChildrenTexts.join(' ')}]`;
-  }
-  const secondCellText = Array.from(firstRowTds[1].childNodes)
-  .filter(node => !node.className || !node.className.includes('dsense'))
-  .map(node => node.textContent)
-  .join('');
-  if (secondCellText) {
-    frontText += ` (${secondCellText.trim()})`;
-  }
-  return frontText;
-}
-
-
-function extractBackText(trGroup) {
-  return `${getLanguageCodes()[1].toUpperCase()}\n` + trGroup
-  .filter(tr => !(parseInt(tr.querySelector('td:last-child').getAttribute('colspan')) > 1))
-  .map(tr => {
-    const tds = tr.querySelectorAll('td');
-    const lastTd = tds[2];
-    const lastTdChildren = Array.from(lastTd.childNodes);
-    let backText = lastTdChildren[0].textContent;
-    const firstTdOtherChildren = lastTdChildren.slice(1);
-    if (firstTdOtherChildren.length > 0) {
-      backText += `[${firstTdOtherChildren.map(node => node.innerText)}]`;
-    }
-    const middleTdText = Array.from(tds[1].childNodes)
-    .filter(node => node.className && node.className.includes('dsense'))
-    .map(node => node.textContent)
-    .join('');
-    if (middleTdText) {
-      backText += ` ${middleTdText}`;
-    }
-    return backText;
-  })
-  .join('\n');
-}
-
-
-
-function addHooksInTrGroup(trGroup) {
-  const hook = createHook(extractFrontText(trGroup), extractBackText(trGroup), trGroup);
-  const parent = trGroup[0].querySelector('td');
-  parent.style.position = 'relative';
-  parent.prepend(hook);
-}
-
-
-function addHooksInTable(tableNode){
-  getTrGroups(tableNode).forEach(addHooksInTrGroup);
-}
-
-
-function getTables() {
-  return document.querySelectorAll('.WRD');
-}
-
 
 function run(){
   appendStyleSheep();
-  getTables().forEach(addHooksInTable);
+
+  // FIXME
+  const hook = createHook('front face text', 'back face text');
+  document.querySelector('hook-parent-selector').append(hook);
+  // !FIXME
 }
 
 
