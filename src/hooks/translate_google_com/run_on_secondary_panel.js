cb0fb905c612292b05ee354a2980bbfca28ff237
Pascal Heitz <oodelally.npm@gmail.com>
2021-01-26 15:28:38 +0100

    Google Translate secondary panel update

--- src/hooks/translate_google_com/run_on_secondary_panel.js
+++ src/hooks/translate_google_com/run_on_secondary_panel.js
@@ -1,34 +1,47 @@
 import highlightOnHookHover from '../../helpers/highlight_on_hook_hover';
-import { querySelector, querySelectorAll, doesAnkiHookExistIn } from '../../helpers/scraping';
+import { querySelectorAll, doesAnkiHookExistIn } from '../../helpers/scraping';
 import { getSourceLanguage, getTargetLanguage } from './get_languages';
 
 
 const tryAddingToRow = (parentTr, createHook) => {
-  const [, , actionTd] = querySelectorAll(parentTr, 'td');
-  if (doesAnkiHookExistIn(actionTd)) {
+  let sourceText;
+  let targetText;
+  let actionContainer;
+  const { children } = parentTr;
+  if (children.length === 3) {
+    [sourceText, targetText, actionContainer] = children;
+  } else if (children.length === 4) {
+    [, sourceText, targetText, actionContainer] = children;
+  } else {
+    return;
+  }
+
+  if (doesAnkiHookExistIn(actionContainer)) {
     return;
   }
   const hook = createHook(() => {
     // The secondary panel proposes reverse translations
     const targetLanguage = getSourceLanguage();
     const sourceLanguage = getTargetLanguage();
-    const [sourceTd, targetTd] = querySelectorAll(parentTr, 'td');
     return {
-      frontText: sourceTd.innerText,
-      backText: targetTd.innerText,
+      frontText: sourceText.innerText,
+      backText: targetText.innerText,
       frontLanguage: sourceLanguage,
       backLanguage: targetLanguage,
       cardKind: `${sourceLanguage} -> ${targetLanguage}`,
     };
   });
-  hook.style.display = 'inline-block';
-  hook.style.marginLeft = '5px';
+  hook.style.position = 'absolute';
+  hook.style.top = '5px';
+  hook.style.right = '50px';
   highlightOnHookHover(hook, parentTr, '#d2e3fc');
-  querySelector(actionTd, '.gt-baf-cell').append(hook);
+  actionContainer.style.position = 'relative';
+  actionContainer.prepend(hook);
 };
 
 
 export default (createHook) => {
-  querySelectorAll(document, '.gt-baf-table tr.gt-baf-entry', { throwOnUnfound: false })
+  querySelectorAll(document, 'tbody tr', { throwOnUnfound: false })
+    .filter(tr => tr.children.length >= 3 && tr.children.length <= 4)
     .forEach(tr => tryAddingToRow(tr, createHook, getSourceLanguage, getTargetLanguage));
 };
