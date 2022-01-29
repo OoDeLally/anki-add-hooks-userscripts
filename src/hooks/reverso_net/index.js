db3ffd8852f8c4302393cdb8997c6f053561a91e
Pascal Heitz <oodelally.npm@gmail.com>
2022-01-29 10:45:06 +0100

    reverso: updated context dictionary

--- src/hooks/reverso_net/index.js
+++ src/hooks/reverso_net/index.js
@@ -1,5 +1,5 @@
 // @name         Anki Add Hooks for Reverso
-// @version      3.0
+// @version      3.1
 // @description  Generate a hook for AnkiConnect on Reverso
 // @author       Pascal Heitz
 // @include      /reverso\.net\/(\w+\/)?\w+-\w+\/
--- src/hooks/reverso_net/run_on_context_reverso.js
+++ src/hooks/reverso_net/run_on_context_reverso.js
@@ -1,9 +1,22 @@
 import highlightOnHookHover from '../../helpers/highlight_on_hook_hover';
 import stringifyNodeWithStyle from '../../helpers/stringify_node_with_style';
-import { querySelector, querySelectorAll } from '../../helpers/scraping';
-import getLanguages from './get_languages';
+import { querySelector, querySelectorAll, } from '../../helpers/scraping';
+import ScrapingError from '../../scraping_error';
 
 // e.g. https://context.reverso.net/traduction/anglais-francais/hello
+
+
+const getLanguages = () => {
+  const match = window.location.href.match(/reverso\.net\/(\w+\/)?([a-z]+)-([a-z]+)\//);
+  if (!match) {
+    throw ScrapingError('Could not extract languages from url');
+  }
+  const [,, sourceLanguage, targetLanguage] = match;
+  if (!sourceLanguage || !targetLanguage) {
+    throw ScrapingError('Could not extract languages from url');
+  }
+  return [sourceLanguage, targetLanguage];
+};
 
 const extractFrontText = containerDiv =>
   stringifyNodeWithStyle(querySelector(containerDiv, '.src > .text'));
@@ -31,9 +44,10 @@ export default (createHook) => {
           cardKind: `${sourceLanguage} -> ${targetLanguage}`,
         };
       });
+      hook.style.position = 'relative';
+      hook.style.top = 'auto';
       hook.style.float = 'right';
       highlightOnHookHover(hook, containerDiv, 'lightblue');
-      containerDiv.style.position = 'relative';
       containerDiv.querySelector('.options .trg').prepend(hook);
     });
 };
